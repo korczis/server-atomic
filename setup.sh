7dc93b8768aee2fe28515a98a5f0e4616092f781
Tomas Korcak <korczis@gmail.com>
2019-02-07 11:24:14 +0100

    WIP

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #! /usr/bin/env sh
 
-SSH_KEY_PATH=/root/.ssh/id_rsa
+SSH_KEY_PATH = /root/.ssh/id_rsa
 
 # Slow the RAID resync down
 # https://serverfault.com/questions/216508/how-to-interrupt-software-raid-resync
 sysctl -w dev.raid.speed_limit_max=1000
 
-rm -rf ${SSH_KEY_PATH}
+rm -rf $SSH_KEY_PATH
 
 # Generate key if not exists
 if [[ ! -f $SSH_KEY_PATH ]]; then
