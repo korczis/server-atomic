a5f77ce77be7a83ff8d8ad3d2d1335a3630a915b
Tomas Korcak <korczis@gmail.com>
2019-02-07 11:18:34 +0100

    Minimalistic setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,12 @@
 #! /usr/bin/env sh
 
-echo "server atomic"
+SSH_KEY_PATH=~/.ssh/id_rsa
 
-mkdir -p ~/.ssh
+# Slow the RAID resync down
+# https://serverfault.com/questions/216508/how-to-interrupt-software-raid-resync
+sysctl -w dev.raid.speed_limit_max=1000
 
-ssh-keygen -N "" -f ~/.ssh/id_rsa -t rsa -b 4096 -C "korczis+proton@gmail.com"
-
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCu0w8+zEfKGl//hGj1DwIjn+Gq6FxY3t1Zy8Sf+X9SCv8C+jt8ALxCIP1FJ6UZAahKnXF7y1rsTavNYHz7qLoIJRcg1/qmyi4uo7T5rbBm+jLkcXBxKXYMmgScgyXcfiEHY1Ht7KJSMryLdqeYonIDu1yEfLoAHIMUTUCyttcZ9j4VkxRGJDlb2Zt4wnfSVQMDgMsY4wwNb2LKug5SZpm3IThoqgsGZIscMIofVAOo/ksJBF2iyoQtttDJ+b98srbClimRoltk56OLkIWkl0l2Erj5yTBv9BDj91qcVp44iCqxzjiUqb0LsQaMrY24IT4lS5P1uGsz7UZS6vKLlM1jmZEdvCAvtvKIkhxZDGjTGKPf1B/4npsm50vke7gky1vy4BQ2OIUHOX2BsfZfx9FKiFKX7Li9vdytskCGQBgf84sGDuiP3CNQA4zmWGtzfIM3M68GBn9AvScOjskHmx3sm5KTZMakW1JSGJNrj+hxnRds9P+JVi8YQRNc3KZMxfnd2phX8itGzN966eSvGPDldsaJ3wClEdtTijA2jqQ96QMBEggUWbDHJE/BCCPGfanVqyFppzovZmKKW89nLsh0xlv4CMlDQQ/0eVlkuzHbo7ZlpsKtGBw+fnNiOMCJgZxD2UCsi2MLfNW+mfjAWAPHthBFbSIfz5XuCtqG0Dm59w== korczis@gmail.com" \
-    > ".ssh/authorized_keys"
-
-chmod 600 ~/.ssh/authorized_keys
-
-systemctl restart sshd
+# Generate key if not exists
+if [[ ! -f ${SSH_KEY_PATH} ]]; then
+    ssh-keygen -N "" -f ${SSH_KEY_PATH} -t rsa -b 4096
+fi
