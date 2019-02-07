bd16a17b4fc41da512281309a93926b3d97457e6
Tomas Korcak <korczis@gmail.com>
2019-02-07 11:28:32 +0100

    WIP

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env sh
 
 SSH_KEY_PATH=/root/.ssh/id_rsa
+SSH_AUTHORIZED_KEYS_PATH=/root/.ssh/authorized_keys
+SSH_AUTHORIZED_KEYS_URL="https://raw.githubusercontent.com/korczis/server-atomic/master/fs/root/.ssh/authorized_keys"
 
 # Slow the RAID resync down
 # https://serverfault.com/questions/216508/how-to-interrupt-software-raid-resync
@@ -9,4 +11,9 @@ sysctl -w dev.raid.speed_limit_max=1000
 # Generate key if not exists
 if [[ ! -f ${SSH_KEY_PATH} ]]; then
     ssh-keygen -N "" -f ${SSH_KEY_PATH} -t rsa -b 4096
+fi
+
+# Create authorized_keys if not exists
+if [[ ! -f ${SSH_AUTHORIZED_KEYS_PATH} ]]; then
+    curl ${SSH_AUTHORIZED_KEYS_URL}
 fi
